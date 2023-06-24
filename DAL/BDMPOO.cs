4296ead4eeaadf63b07bde5e1d4af039329b2f2a
Jader18 <mendozajader18@gmail.com>
2023-06-23 18:44:54 -0600

    Agregar archivos de proyecto.

---
+++ DAL/BDMPOO.cs
@@ -0,0 +1,32 @@
+﻿using EL;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace DAL
+{
+    public class BDMPOO : DbContext 
+    {
+        public BDMPOO() : base(Conexion.ConexionString(true)) { }
+        public virtual DbSet<Formularios> Formularios { get; set; }
+        public virtual DbSet<Formularios> Permisos { get; set; }
+        public virtual DbSet<Formularios> Roles { get; set; }
+        public virtual DbSet<Formularios> RolFormularios { get; set; }
+        public virtual DbSet<Formularios> RolPermisos { get; set; }
+        public virtual DbSet<Formularios> Usuarios { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Formularios>().Property(e => e.Formulario).IsUnicode(false);          
+            modelBuilder.Entity<Permisos>().Property(e => e.Permiso).IsUnicode(false);
+            modelBuilder.Entity<Roles>().Property(e => e.Rol).IsUnicode(false);
+            modelBuilder.Entity<Usuarios>().Property(e => e.NombreCompleto).IsUnicode(false);
+            modelBuilder.Entity<Usuarios>().Property(e => e.Correo).IsUnicode(false);
+            modelBuilder.Entity<Usuarios>().Property(e => e.UserName).IsUnicode(false);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder); 
+
+        }
+    }
+}
---
+++ DAL/Conexion.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+
+
+namespace DAL
+{
+    public static class Conexion
+    {
+        private static string NombreAplicacion =
+        private static string Servidor = @"Jader Mendoza\SQL2019";
+        private static string Usuario = "";
+        private static string Password = "";
+        private static string BaseDatos = "MPOO";
+
+
+        public static string ConexionString(bool SqlAutentication = true)
+        {
+            SqlConnectionStringBuilder Constructor = new SqlConnectionStringBuilder()
+            {
+                ApplicationName = NombreAplicacion,
+                IntegratedSecurity = !SqlAutentication,
+                DataSource = Servidor,
+                InitialCatalog = BaseDatos
+            };
+            if (SqlAutentication)
+            {
+                Constructor.UserID = Usuario;
+                Constructor.Password = Password;
+            }
+            return Constructor.ConnectionString;
+        }
+
+    }
+}
